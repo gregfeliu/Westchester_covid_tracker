b865a4396800521a2d72a269c0520fb497ddbed8
Gregory Feliu <36009037+gregfeliu@users.noreply.github.com>
2021-02-17 10:48:08 -0500

    Add files via upload

--- scripts/retrieve_update_data.py
+++ scripts/retrieve_update_data.py
@@ -2,7 +2,7 @@
 # coding: utf-8
 
 
-# ## Imports
+# Imports
 import git
 import pandas as pd
 import seaborn as sns
@@ -12,44 +12,43 @@ import numpy as np
 import warnings
 warnings.filterwarnings('ignore')
 
-# Update the data
+
+
+# Update the data from the source https://github.com/nytimes/covid-19-data 
 repo = git.Repo('../covid-19-data')
 repo.remotes.origin.pull()
 
 # Engineer the data
-
-
 us_counties = pd.read_csv("../covid-19-data/us-counties.csv", infer_datetime_format=True)
 westchester_data = us_counties[us_counties['county'] == 'Westchester']
 westchester_data.drop(columns = 'fips', inplace=True)
 westchester_data.reset_index(inplace=True)
 
-# ## Scaling Cases Data
-# pop of westchester is 967506
+## Scaling Cases Data
+### pop of westchester is 967506
 westchester_pop = 967506
 scale_to_100k = westchester_pop / 100000
 westchester_data['total_cases_per_100k'] = westchester_data['cases'] / scale_to_100k
 
 
-# ### New cases per capita over past
-# new cases column
+## New cases per capita over past week
 new_cases = [westchester_data['total_cases_per_100k'][idx+1] - 
                               westchester_data['total_cases_per_100k'][idx] for idx in range(len(westchester_data)-1)]
 new_cases.append(np.nan)
 westchester_data['new_cases_per_cap'] = new_cases
-
 westchester_data['new_cases_per_cap_past_7_days'] = westchester_data['new_cases_per_cap'].rolling(window=7, min_periods=0).sum()
 
-
-# ## Saving Data
+## Saving Data
 westchester_cases = westchester_data.to_csv("../westchester_coronavirus.csv")
 
-# adding new columns
+
+# Plotting the data 
+## adding new date columns
 westchester_data['dates_datetime'] = pd.to_datetime(westchester_data['date'], infer_datetime_format=True)
 latest_date = westchester_data.iloc[-1]['date']
 print(f"The latest date in this dataset is: {latest_date}")
 
-# functions to help plot data
+## functions to help plot data
 def make_lineplot(x_col, y_col, x_col_name, y_col_name, title):
     ax = sns.lineplot(data=westchester_data, x=x_col, y=y_col)
     ax.tick_params(axis='x', labelrotation = 315, size = 12)
@@ -64,9 +63,9 @@ def make_save_lineplot(x_col, y_col, x_col_name, y_col_name, title, new_title):
     fig.savefig("../images/" + new_title, bbox_inches ='tight')
 
 
-# Plot and save new cases over past 7 days per 100k people plot
-make_save_lineplot("dates_datetime", "new_cases_per_cap_past_7_days", 'Date', 
+## Plot and save new cases over past 7 days per 100k people plot
+make_save_lineplot("dates_datetime", "new_cases_per_cap_past_7_days",
+                   'Date', 
                     'New Cases Per Cap Past 7 Days',
-                    "Number of New Cases in Westchester, NY Over Past 7 Days (Per 100,000 People)", 
+                    "Number of New Cases in Westchester,NY Over Past 7 Days (Per 100,000 People)", 
                    'Plot_of_New_Cases_Per_Past_7_Days.png')
-
